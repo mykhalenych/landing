ec002003dedb437be093b6132900ecc10eee145c
mykhalenych <mykhalenych@gmail.com>
2025-06-17 19:41:12 +0300

    added header

---
+++ main.js
@@ -0,0 +1,7 @@
+const button = document.querySelector('.humburher-wrap');
+const block = document.querySelector('.mobile-navigation');
+
+
+button.addEventListener('click', (e) => {
+    block.classList.toggle('active');
+})
